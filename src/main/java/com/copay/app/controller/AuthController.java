6b7a8dac707b0db544da2b1da4e3faa401b1374d
Dynavy <dynavy@hotmail.com>
2025-03-06 18:10:14 +0100

    FEAT(AuthController): Added functionalities to fix the validations
    - Implemented @Valid annotation in the parameters.
    
    - Addded the BindingResult result in the parameters + an if to check if the validations had been triggered.

--- src/main/java/com/copay/app/controller/AuthController.java
+++ src/main/java/com/copay/app/controller/AuthController.java
@@ -2,34 +2,44 @@ package com.copay.app.controller;
 
 import com.copay.app.dto.UserRegisterRequest;
 import com.copay.app.service.UserService;
+
+import jakarta.validation.Valid;
+
 import com.copay.app.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @Autowired
-    private JwtService jwtService;
+	@Autowired
+	private JwtService jwtService;
 
-    @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody UserRegisterRequest userRegisterRequest) {
-        try {
-            userService.registerUser(userRegisterRequest);
+	@PostMapping("/register")
+	public ResponseEntity<?> registerUser(@RequestBody @Valid UserRegisterRequest userRegisterRequest,
+			BindingResult result) {
 
-            // Generates a token for the specific username.
-            String token = jwtService.generateToken(userRegisterRequest.getUsername());
+		// Validate the @Annotations from UserRegisterRequest.
+		if (result.hasErrors()) {
+			return ResponseEntity.badRequest().body("Error: " + result.getAllErrors().get(0).getDefaultMessage());
+		}
 
-            return ResponseEntity.ok().body("JWT Token: " + token);
-            
-        } catch (Exception e) {
-        	// HTTP 400 -> BAD REQUEST.
-            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
-        }
-    }
+		try {
+			userService.registerUser(userRegisterRequest);
+
+			// Generates a token for the specific user.
+			String token = jwtService.generateToken(userRegisterRequest.getUsername());
+
+			return ResponseEntity.ok().body("JWT Token: " + token);
+			
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+		}
+	}
 }
