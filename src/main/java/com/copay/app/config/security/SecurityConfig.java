77845cfdf83bd1bc433421132ec6b6f765833d71
Dynavy <dynavy@hotmail.com>
2025-03-13 16:11:02 +0100

    REFACTOR(SecurityConfig): added logic for the JWT.
    - Added lines related to the JWT configuration.
    
    - Formatted and comented important lines.

--- src/main/java/com/copay/app/config/security/SecurityConfig.java
+++ src/main/java/com/copay/app/config/security/SecurityConfig.java
@@ -4,17 +4,16 @@ import java.util.Base64;
 
 import javax.crypto.spec.SecretKeySpec;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -22,6 +21,7 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.copay.app.service.CustomUserDetailsService;
 import com.copay.app.service.JwtService;
 
 import io.github.cdimascio.dotenv.Dotenv;
@@ -32,29 +32,29 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class SecurityConfig {
 
 	Dotenv dotenv = Dotenv.load();
+
 	String jwtSecret = dotenv.get("JWT_SECRET");
 
-    private final JwtService jwtService;
-    private final UserDetailServiceImpl userDetailServiceImpl;
+	private final JwtService jwtService;
 	private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
-	public SecurityConfig(CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
+	// Constructor to initialize JwtService and CustomAuthenticationEntryPoint
+	// dependencies.
+	public SecurityConfig(JwtService jwtService, CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
+		this.jwtService = jwtService;
 		this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
 	}
 
 	@Autowired
-	private UserDetailServiceImpl userDetailServiceImpl;
+	private CustomUserDetailsService customUserDetailsService;
 
-    public SecurityConfig(JwtService jwtService, UserDetailServiceImpl userDetailServiceImpl) {
-        this.jwtService = jwtService;
-        this.userDetailServiceImpl = userDetailServiceImpl;
-    }
-    
+	// Bean to create a password encoder using BCrypt algorithm.
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
+	// Bean to create a JWT decoder using a secret key for token verification.
 	@Bean
 	public JwtDecoder jwtDecoder() {
 		byte[] secretBytes = Base64.getDecoder().decode(jwtSecret);
@@ -63,36 +63,52 @@ public class SecurityConfig {
 		return NimbusJwtDecoder.withSecretKey(key).build();
 	}
 
+	// Bean to configure the security filter chain for HTTP requests.
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-	    // Create an instance of the JwtAuthenticationFilter with the JwtService
-	    JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtService);
-	    
-	    http.csrf(csrf -> csrf.disable()) // Disable CSRF protection
-	        .authorizeHttpRequests(auth -> auth
-	            .requestMatchers("/api/response", "/api/auth/register", "/api/auth/login", "/api/users/**").permitAll() // Allow public routes
-	            .anyRequest().authenticated()) // Any other requests require authentication
-	        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Disable session management 
-			// Custom error message for unauthenticated requests.
-			.exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(customAuthenticationEntryPoint))
-			// Enable JWT-based OAuth2 resource server support.
-			.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-			// Enable CORS (Cross-Origin Resource Sharing) with default settings
-	        .cors(Customizer.withDefaults()) 
-			// Add the JWT filter before the default authentication filter.
-	        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); 
 
-		// Return the configured SecurityFilterChain
-	    return http.build(); 
+		JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtService);
+
+		// Disable CSRF protection.
+		http.csrf(csrf -> csrf.disable())
+
+				// Enable CORS with default settings.
+				.cors(Customizer.withDefaults())
+
+				// Allow public routes.
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/api/response", "/api/auth/register", "/api/auth/login", "/api/users/**")
+						.permitAll()
+
+						// Require authentication for all other endpoints.
+						.anyRequest().authenticated())
+
+				// Set session policy to stateless.
+				.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+				// Custom error for unauthenticated requests.
+				.exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(customAuthenticationEntryPoint))
+
+				// Enable JWT-based OAuth2 resource server.
+				.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+
+				// Add JWT filter before authentication filter.
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
 	}
 
+	// Bean to create the AuthenticationManager to authenticate users.
 	@Bean
-	public AuthenticationManager authenticationManager(HttpSecurity http, 
-	                                                   PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) throws Exception {
-	    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-	    authProvider.setPasswordEncoder(passwordEncoder);
-	    authProvider.setUserDetailsService(userDetailsService);
+	public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
 
-	    return new ProviderManager(authProvider);
+		AuthenticationManagerBuilder authenticationManagerBuilder = http
+				.getSharedObject(AuthenticationManagerBuilder.class);
+
+		// Configures AuthenticationManager to load user details and verify password.
+		authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+
+		// Return the AuthenticationManager.
+		return authenticationManagerBuilder.build();
 	}
 }
