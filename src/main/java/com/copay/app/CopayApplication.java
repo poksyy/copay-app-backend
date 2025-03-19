dfd72ca91c0afe3834407a3985556c802e8122f9
Dynavy <dynavy@hotmail.com>
2025-03-19 16:19:20 +0100

    REFACTOR(main): environment variable loading logic.
    - Moved the environment variable loading logic from the `CopayApplication` main class to a dedicated `EnvLoader` class.
    
    - The `EnvLoader` class now handles the loading of the `.env` file and setting the environment variables as system properties.
    
    - Simplified the `CopayApplication` class by calling the static `loadEnvVariables` method during application startup.

--- src/main/java/com/copay/app/CopayApplication.java
+++ src/main/java/com/copay/app/CopayApplication.java
@@ -3,23 +3,18 @@ package com.copay.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.copay.app.config.env.EnvLoader;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class CopayApplication {
 
     public static void main(String[] args) {
-    	
-        // Load the .env file and ignore it if missing (prevents errors in non-local environments)
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        // Load the .env file through EnvLoad class.
+        EnvLoader.loadEnvVariables();
 
-        // Set database and security environment variables as system properties
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-        
-        // Start the Spring Boot application
+        // Start the Spring Boot application.
         SpringApplication.run(CopayApplication.class, args);
     }
 }
