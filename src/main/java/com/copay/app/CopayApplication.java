e971f7dd6ac1870669652e09772a43ac53f44ad5
Cristian Oraña <krischxn@gmail.com>
2025-03-20 00:26:27 +0100

    CHORE(App): Enable scheduling for periodic tasks.

--- src/main/java/com/copay/app/CopayApplication.java
+++ src/main/java/com/copay/app/CopayApplication.java
@@ -2,12 +2,14 @@ package com.copay.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.copay.app.config.env.EnvLoader;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
+@EnableScheduling
 public class CopayApplication {
 
     public static void main(String[] args) {
