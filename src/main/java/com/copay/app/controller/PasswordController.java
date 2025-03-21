2dff2f0708f144b91b750e3750b4bc046e9ed902
Pau Carrera <153611745+poksyy@users.noreply.github.com>
2025-03-21 12:23:37 +0100

    FIX(URL): Fixed email url in forgot-password endpoint.

--- src/main/java/com/copay/app/controller/PasswordController.java
+++ src/main/java/com/copay/app/controller/PasswordController.java
@@ -1,20 +1,24 @@
 package com.copay.app.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.copay.app.dto.password.ForgotPasswordRequest;
+import com.copay.app.dto.password.ForgotPasswordResetRequest;
 import com.copay.app.dto.password.ResetPasswordRequest;
+import com.copay.app.entity.User;
+import com.copay.app.exception.UserNotFoundException;
+import com.copay.app.repository.UserRepository;
 import com.copay.app.service.EmailService;
 import com.copay.app.service.JwtService;
 import com.copay.app.service.password.PasswordService;
@@ -26,41 +30,49 @@ import jakarta.validation.Valid;
 @RequestMapping("/api")
 public class PasswordController {
 
-	@Autowired
-	private PasswordService passwordService;
-	private final EmailService emailService;
-	private final JwtService jwtService;
+    @Autowired
+    private PasswordService passwordService;
+    private EmailService emailService;
+    private JwtService jwtService;
+    private UserRepository userRepository;
 
-	public PasswordController(EmailService emailService, JwtService jwtService) {
-		this.emailService = emailService;
-		this.jwtService = jwtService;
-	}
+    public PasswordController(EmailService emailService, JwtService jwtService, UserRepository userRepository) {
+        this.emailService = emailService;
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
 
-	@PutMapping("/{id}/reset-password")
-	public ResponseEntity<?> resetPassword(@PathVariable Long id,
-			@RequestBody @Valid ResetPasswordRequest passwordUpdateRequest,
-			@RequestHeader("Authorization") String authorizationHeader) {
+    // Resets the user's password from within the app using a valid JWT token and current password.
+    @PutMapping("/{id}/reset-password")
+    public ResponseEntity<?> resetPassword(@PathVariable Long id,
+            @RequestBody @Valid ResetPasswordRequest passwordUpdateRequest,
+            @RequestHeader("Authorization") String authorizationHeader) {
 
-		// Extract the token from the Authorization header
-		String token = authorizationHeader.replace("Bearer ", "");
+        // Extract the token from the Authorization header
+        String token = authorizationHeader.replace("Bearer ", "");
 
-		return passwordService.resetPassword(id, passwordUpdateRequest, token);
-	}
+        // Delegate to the service
+        return passwordService.resetPassword(id, passwordUpdateRequest, token);
+    }
 
+    // Sends a password reset link to the user's email for the "Forgot Password" Login flow.
     @PostMapping("/forgot-password")
-    public ResponseEntity<String> forgotPassword(@RequestBody ForgotPasswordRequest request) {
+    public ResponseEntity<?> forgotPassword(@RequestBody ForgotPasswordRequest request) {
         String email = request.getEmail();
 
-        String token = jwtService.generateToken(email);
+        // Delegate to the service
+        return passwordService.forgotPassword(email);
+    }
 
-        // TODO implement useful url to update the password
-        String resetLink = "" + token;
+    // Resets the user's password via a token from the "Forgot Password" email link.
+    @PutMapping("/forgot-password-reset")
+    public ResponseEntity<?> forgotPasswordReset(@RequestBody @Valid ForgotPasswordResetRequest passwordResetRequest,
+            @RequestHeader("Authorization") String authorizationHeader) {
 
-        try {
-            emailService.sendResetPasswordEmail(email, resetLink);
-            return ResponseEntity.ok("Email sent successfully.");
-        } catch (MessagingException e) {
-            return ResponseEntity.status(500).body("Error sending the email.");
-        }
+        // Extract the token from the Authorization header
+        String token = authorizationHeader.replace("Bearer ", "");
+
+        // Delegate to the service
+        return passwordService.forgotPasswordReset(token, passwordResetRequest);
     }
 }
