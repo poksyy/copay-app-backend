e3058f99d77ed400681a6485027f81af1ff2c7ab
Dynavy <dynavy@hotmail.com>
2025-03-22 10:36:29 +0100

    FEAT(AuthController): Extract token trought the SecurityContextHolder.
    - The token is extracted through he SecurityContextHolder after being authenticated by  the JwtAuthenticationFilter class.
    
    - After extracting it,  the token is sent as an argument to the registerStepTwo() service layer method.

--- src/main/java/com/copay/app/controller/AuthController.java
+++ src/main/java/com/copay/app/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.copay.app.dto.JwtResponse;
 import com.copay.app.dto.UserLoginRequest;
 import com.copay.app.dto.UserRegisterStepTwoDTO;
 import com.copay.app.dto.UserRegisterStepOneDTO;
+import com.copay.app.service.CustomUserDetailsService;
 import com.copay.app.service.ValidationService;
 import com.copay.app.service.auth.AuthService;
 import com.copay.app.validation.ValidationErrorResponse;
@@ -11,7 +12,10 @@ import com.copay.app.validation.ValidationErrorResponse;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,18 +45,21 @@ public class AuthController {
 	// Update phone number of the user.
 	@PostMapping("/register/step-two")
 	public ResponseEntity<?> registerStepTwo(@RequestBody @Valid UserRegisterStepTwoDTO userRegisterStepTwoDTO,
-			@RequestHeader("Authorization") String token, BindingResult result) {
+	        BindingResult result, @RequestHeader("Authorization") String authorizationHeader) {
 
-		// Validates user input.
-		ValidationErrorResponse validationResponse = ValidationService.validate(result);
+	    // Validates user input.
+	    ValidationErrorResponse validationResponse = ValidationService.validate(result);
+	    if (validationResponse != null) {
+	        return ResponseEntity.badRequest().body(validationResponse);
+	    }
+	    
+	    // Get the authentication thought the JwtAuthenticationFilter class.
+	    String token = SecurityContextHolder.getContext().getAuthentication().getCredentials().toString();
 
-		if (validationResponse != null) {
-			return ResponseEntity.badRequest().body(validationResponse);
-		}
+		// Registers the user and returns a JWT response.
+	    JwtResponse jwtResponse = authService.registerStepTwo(userRegisterStepTwoDTO, token);
 
-		authService.registerStepTwo(userRegisterStepTwoDTO, token);
-		
-		return ResponseEntity.ok("Phone number updated successfully");
+	    return ResponseEntity.ok().body(jwtResponse);
 	}
 
 	// Handles user login request.
@@ -62,6 +69,7 @@ public class AuthController {
 		ValidationErrorResponse validationResponse = ValidationService.validate(result);
 
 		if (validationResponse != null) {
+			
 			return ResponseEntity.badRequest().body(validationResponse);
 		}
 
