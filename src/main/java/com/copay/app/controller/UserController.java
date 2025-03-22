92e20a5c1e8ee721c27d912b6cc726dd6dff832f
Dynavy <dynavy@hotmail.com>
2025-03-22 10:31:58 +0100

    CHORE(UserController): Removed unnecesary import.

--- src/main/java/com/copay/app/controller/UserController.java
+++ src/main/java/com/copay/app/controller/UserController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.copay.app.dto.UserRegisterStepTwoDTO;
 import com.copay.app.dto.UserResponseDTO;
 import com.copay.app.dto.user.UserCreateRequest;
 import com.copay.app.dto.user.UserUpdateRequest;
