8899a1b50f1e8d8c53caeec0e163a62a1a9b2056
Dynavy <dynavy@hotmail.com>
2025-03-30 22:13:22 +0200

    FEAT(Groups): Created Group entity with relationships
    - Added Group entity with the following fields: groupId, groupName, createdBy, and createdAt.
    
    - Defined @ManyToOne relationship with User for tracking the group creator.
    
    - Implemented @ManyToMany relationship with User for group members.
    
    - Included getters and setters for all fields.

---
+++ src/main/java/com/copay/app/entity/Group.java
@@ -0,0 +1,71 @@
+package com.copay.app.entity;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "groups")
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_id")
+    private Long groupId;
+
+    @Column(name = "group_name", nullable = false, length = 100)
+    private String groupName;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false)
+    private User createdBy;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    // ManyToMany relation with user table.
+    @ManyToMany(mappedBy = "groups")
+    private Set<User> users;
+
+    // Getter and Setters.
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
