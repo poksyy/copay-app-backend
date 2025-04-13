71cd6e1ffab001c9091bcbf1beb728ecd07848d5
Dynavy <dynavy@hotmail.com>
2025-04-13 16:40:04 +0200

    CHORE(UserLoginRequestDTO): Improved naming and deleted constructors.
    - Renamed the class from UserLoginRequest to UserLoginRequestDTO.
    
    - Deleted useless constructors.

--- src/main/java/com/copay/app/dto/auth/UserLoginRequestDTO.java
+++ src/main/java/com/copay/app/dto/auth/UserLoginRequestDTO.java
@@ -2,23 +2,13 @@ package com.copay.app.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
 
-public class UserLoginRequest {
+public class UserLoginRequestDTO {
 
 	@NotBlank(message = "Phone number cannot be empty")
 	private String phone_number;
 
 	@NotBlank(message = "Password cannot be empty")
 	private String password;
-
-	// Constructor for deserialization.
-	public UserLoginRequest() {
-	}
-
-	public UserLoginRequest(String phone_number, String password) {
-		this.phone_number = phone_number;
-		this.password = password;
-
-	}
 
 	// Getters and Setters.
 	public String getPhoneNumber() {
