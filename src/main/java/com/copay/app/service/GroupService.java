3e5226ff8c6f054ecad904e34f386e76aacc007a
Dynavy <dynavy@hotmail.com>
2025-04-13 16:26:36 +0200

    REFACTOR(GroupService): Changed createGroup() method.
    - **Reordered validation:** Moved validation of existing Copay member phone numbers to the beginning of the method, immediately after validating the creator user.
    
    - **Removed unused methods:** Removed unnecessary calls to `group.setImageUrl()` and `group.setImageProvider()`, which will be implemented later.
    
    - **Clarified logic:** Added comments to clarify the logic for creating and saving `GroupMember` entities, particularly for the line where the `GroupMemberId` is created.
    
    - **Optimized phone number validation:** Replaced exception throwing for duplicate user phone numbers with `continue` to skip already existing users and proceed with the next in the loop.
    
    - **Optimized save logic:** Replaced direct repository save calls for `GroupMember` and `ExternalMember` entities with `add()` methods to reduce unnecessary repository calls and leverage cascading.
    
    - **Ensured entity synchronization:** Added `entityManager.merge(group)` to explicitly merge changes to the `Group` entity, ensuring synchronization with the database and proper flushing of the persistence context. This step is necessary since `repository.save()` is not used for the `Group` entity.
    
    - **Simplified external member handling:** Applied the same logic for adding external members to the group, using the `add()` method on `group.getExternalMembers()` instead of repository calls.
    
    - **Entity merge for consistency:** Used `entityManager.merge(group)` to flush changes and resolve potential conflicts.
    
    - **Helper method for DTO conversion:** Used the helper method `mapToGroupResponseDTO` to convert the `Group` entity into a `CreateGroupResponseDTO`, returned as the backend response with updated group details.
    
      - **Key information extracted:**
        - Group ID, name, description, and owner details.
        - Registered members and external members.
    
      - **Efficiency and relationships:**
        - The relationships between `Group`, `GroupMember`, `ExternalMember`, and `User` simplify data retrieval and ensure efficient handling of the response structure.

--- src/main/java/com/copay/app/service/GroupService.java
+++ src/main/java/com/copay/app/service/GroupService.java
@@ -1,27 +1,34 @@
 package com.copay.app.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.copay.app.dto.group.GroupRequestDTO;
-import com.copay.app.dto.responses.GroupResponseDTO;
+import com.copay.app.dto.group.CreateGroupRequestDTO;
+import com.copay.app.dto.group.GetGroupRequestDTO;
+import com.copay.app.dto.group.auxiliary.CopayMemberDTO;
+import com.copay.app.dto.group.auxiliary.ExternalMemberDTO;
+import com.copay.app.dto.group.auxiliary.GroupOwnerDTO;
+import com.copay.app.dto.responses.GetGroupResponseDTO;
+import com.copay.app.dto.responses.CreateGroupResponseDTO;
 import com.copay.app.entity.Group;
 import com.copay.app.entity.User;
+import com.copay.app.entity.relations.ExternalMember;
 import com.copay.app.entity.relations.GroupMember;
 import com.copay.app.entity.relations.GroupMemberId;
-import com.copay.app.exception.UserAlreadyMemberException;
+import com.copay.app.exception.InvitedMemberNotFoundException;
+import com.copay.app.exception.UserNotFoundException;
+import com.copay.app.repository.ExternalMemberRepository;
 import com.copay.app.repository.GroupMemberRepository;
 import com.copay.app.repository.GroupRepository;
 import com.copay.app.repository.UserRepository;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityNotFoundException;
+
 import jakarta.persistence.PersistenceContext;
 
 @Service
@@ -33,108 +40,141 @@ public class GroupService {
 	@Autowired
 	private GroupMemberRepository groupMemberRepository;
 
-	// Injection of UserRepository to find users by phoneNumber.
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private ExternalMemberRepository externalMemberRepository;
 
 	// Use EntityManager to obtain a reference to the User entity
 	@PersistenceContext
 	private EntityManager entityManager;
 
-    @Transactional
-    public GroupResponseDTO createGroup(GroupRequestDTO request) {
-
-        // Get a reference to the creator user
-        User creator = entityManager.getReference(User.class, request.getCreatedBy());
-
-        // Create the group entity
-        Group group = new Group();
-        group.setGroupName(request.getGroupName());
-        group.setCreatedBy(creator);
-        group.setDescription(request.getDescription());
-        group.setEstimatedPrice(request.getEstimatedPrice());
-        group.setCurrency(request.getCurrency());
-        group.setImageUrl(request.getImageUrl());
-        group.setImageProvider(request.getImageProvider());
-
-        // Persist the group in the database
-        group = groupRepository.save(group);
-
-        // Add the creator as a member
-        GroupMemberId groupMemberId = new GroupMemberId(group, creator);
-        GroupMember groupMember = new GroupMember(groupMemberId);
-        groupMemberRepository.save(groupMember);
-
-        // Add the invited users as group members based on their phone numbers
-        for (String phoneNumber : request.getInvitedMembers()) {
-        	
-            User invitedUser = userRepository.findByPhoneNumber(phoneNumber)
-                    .orElseThrow(() -> new EntityNotFoundException("User with phone number " + phoneNumber + " not found"));
-
-            GroupMemberId invitedGroupMemberId = new GroupMemberId(group, invitedUser);
-            
-            if (groupMemberRepository.existsById(invitedGroupMemberId)) {
-                throw new UserAlreadyMemberException("User with phone number " + phoneNumber + " is already a member");
-            }
-
-            GroupMember invitedGroupMember = new GroupMember(invitedGroupMemberId);
-            groupMemberRepository.save(invitedGroupMember);
-        }
-
-        return mapToGroupResponseDTO(group);
-    }
-
-	@Transactional(readOnly = true)
-	public List<GroupResponseDTO> getGroupsByUser(Long userId) {
-
-		// Retrieve groups by querying the group_members table for the given user.
-		List<GroupMember> groupMembers = groupMemberRepository.findByIdUserUserId(userId);
-		
-		return groupMembers.stream().map(gm -> mapToGroupResponseDTO(gm.getId().getGroup()))
-				.collect(Collectors.toList());
-	}
-
 	@Transactional
-	public GroupResponseDTO joinGroup(Long groupId, Long userId) {
+	public CreateGroupResponseDTO createGroup(CreateGroupRequestDTO request) {
 
-		// Retrieve the group
-		Group group = groupRepository.findById(groupId)
-				.orElseThrow(() -> new EntityNotFoundException("Group not found"));
+		// Check if the creator exists on the database and saves the user_id.
+		User creator = userRepository.findById(request.getCreatedBy())
+				.orElseThrow(() -> new UserNotFoundException("User with ID " + request.getCreatedBy() + " not found"));
 
-		// Get a reference to the user
-		User user = entityManager.getReference(User.class, userId);
-
-		// Check if the user is already a member of the group
-		GroupMemberId groupMemberId = new GroupMemberId(group, user);
-
-		if (groupMemberRepository.existsById(groupMemberId)) {
-			throw new UserAlreadyMemberException("User is already a member of the group");
+		// Loop to check if the invited Copay members have an account.
+		for (String phoneNumber : request.getInvitedCopayMembers()) {
+			userRepository.findByPhoneNumber(phoneNumber).orElseThrow(() -> new InvitedMemberNotFoundException(
+					"This phone number owner doesn't have an account: " + phoneNumber));
 		}
 
-		// Add the user as a member
-		GroupMember groupMember = new GroupMember(groupMemberId);
-		groupMemberRepository.save(groupMember);
+		// Creates a group instance.
+		Group group = new Group();
 
+		// This field will store the reference to the User entity, JPA will handle the
+		// user_id automatically.
+		group.setCreatedBy(creator);
+		group.setGroupName(request.getGroupName());
+		group.setDescription(request.getDescription());
+		group.setEstimatedPrice(request.getEstimatedPrice());
+		group.setCurrency(request.getCurrency());
+
+		// Persists the group details in the database.
+		group = groupRepository.save(group);
+		
+		// Create the composite key for the group-member relationship with the group and user.
+		GroupMemberId creatorMemberId = new GroupMemberId(group, creator);
+		// Instantiate the GroupMember entity using the composite key.
+		GroupMember creatorGroupMember = new GroupMember(creatorMemberId);
+
+		// Persist the new group member to the repository, saving the relationship.
+		groupMemberRepository.save(creatorGroupMember);
+
+		// Loop to persist invited copay members into the database.
+		for (String phoneNumber : request.getInvitedCopayMembers()) {
+
+			// Find the users by the phone number.
+			User invitedCopayMember = userRepository.findByPhoneNumber(phoneNumber).get();
+
+			GroupMemberId invitedMemberId = new GroupMemberId(group, invitedCopayMember);
+
+			// Validates if the user already belongs to the group.
+			if (groupMemberRepository.existsById(invitedMemberId)) {
+				// Skip the user if they are already a member of the group.
+				continue;
+			}
+
+			// If the user is not already a member, create and add them to the group.
+			GroupMember invitedGroupMember = new GroupMember(invitedMemberId);
+
+			// Add the user to the group
+			group.getGroupMembers().add(invitedGroupMember);
+		}
+
+		// Loop to interact with the List of the externalMembers.
+		for (String externalName : request.getInvitedExternalMembers()) {
+
+			// Create and persist an ExternalMember.
+			ExternalMember externalMember = new ExternalMember();
+			externalMember.setName(externalName);
+			externalMember.setGroup(group);
+			externalMember.setJoinedAt(LocalDateTime.now());
+
+			// Add the external member to the group
+			group.getExternalMembers().add(externalMember);
+		}
+
+		// Explicitly merge the group to flush the changes and resolve potential
+		// conflicts.
+		entityManager.merge(group);
+
+		// Map the group instance to GroupResponseDTO.
 		return mapToGroupResponseDTO(group);
 	}
 
 	// Helper method to map Group entity to GroupResponseDTO.
-	private GroupResponseDTO mapToGroupResponseDTO(Group group) {
-	    
-	    GroupResponseDTO groupResponseDTO = new GroupResponseDTO();
-	    
-	    // Set group details.
-	    groupResponseDTO.setGroupId(group.getGroupId());
-	    groupResponseDTO.setGroupName(group.getGroupName());
-	    groupResponseDTO.setCreatedBy(group.getCreatedBy().getUserId());
-	    groupResponseDTO.setCreatedAt(group.getCreatedAt());
+	private CreateGroupResponseDTO mapToGroupResponseDTO(Group group) {
 
-	    // Set member IDs if group has members.
-	    if (group.getGroupMembers() != null) {
-	        groupResponseDTO.setMemberIds(group.getGroupMembers().stream()
-	            .map(gm -> gm.getId().getUser().getUserId())
-	            .collect(Collectors.toList()));
-	    }
-	    return groupResponseDTO;
+		// Initialize an instance of the DTO that is going to be used as a response.
+		CreateGroupResponseDTO groupResponseDTO = new CreateGroupResponseDTO();
+
+		// Set group details
+		groupResponseDTO.setGroupId(group.getGroupId());
+		groupResponseDTO.setGroupName(group.getGroupName());
+		groupResponseDTO.setDescription(group.getDescription());
+		groupResponseDTO.setEstimatedPrice(group.getEstimatedPrice());
+		groupResponseDTO.setCurrency(group.getCurrency());
+		groupResponseDTO.setCreatedAt(group.getCreatedAt());
+
+		// Map group owner details (GroupOwnerDTO-> includes id and username).
+		GroupOwnerDTO groupOwnerDTO = new GroupOwnerDTO();
+
+		// Is possible to use the getters of Group and User entity thanks to the relations.
+		groupOwnerDTO.setOwnerId(group.getCreatedBy().getUserId());
+		groupOwnerDTO.setOwnerName(group.getCreatedBy().getUsername());
+		groupResponseDTO.setGroupOwner(groupOwnerDTO);
+
+		// Map registered members (CopayMemberDTO -> includes id and phoneNumber).
+		if (group.getGroupMembers() != null) {
+
+			// Convert each group member to CopayMemberDTO.
+			List<CopayMemberDTO> groupMemberResponseDTOList = group.getGroupMembers().stream().map(gm -> {
+				User user = gm.getId().getUser();
+				return new CopayMemberDTO(user.getUserId(), user.getPhoneNumber());
+			}).collect(Collectors.toList());
+
+			// Set the mapped list of CopayMemberDTOs in the response DTO.
+			groupResponseDTO.setCopayMembers(groupMemberResponseDTOList);
+		}
+
+		// Map external members (ExternalMemberDTO -> includes externalMembersId and name).
+		if (group.getExternalMembers() != null) {
+
+			// Convert each external member to ExternalMemberDTO.
+			List<ExternalMemberDTO> externalList = group.getExternalMembers().stream()
+					.map(externalMember -> new ExternalMemberDTO(externalMember.getExternalMembersId(),
+							externalMember.getName()))
+					.collect(Collectors.toList());
+
+			// Set the mapped list of ExternalMemberDTOs in the response DTO.
+			groupResponseDTO.setExternalMembers(externalList);
+		}
+		
+		return groupResponseDTO;
 	}
 }
