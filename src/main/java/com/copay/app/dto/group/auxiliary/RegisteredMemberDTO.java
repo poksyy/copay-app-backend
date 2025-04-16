899b55a817aee6f197da83d0d41f35fcf588889c
Dynavy <dynavy@hotmail.com>
2025-04-16 17:18:19 +0200

    CHORE(CopayMemberDTO): Renamed DTO file name

--- src/main/java/com/copay/app/dto/group/auxiliary/RegisteredMemberDTO.java
+++ src/main/java/com/copay/app/dto/group/auxiliary/RegisteredMemberDTO.java
@@ -1,12 +1,12 @@
 package com.copay.app.dto.group.auxiliary;
 
-public class CopayMemberDTO {
+public class RegisteredMemberDTO {
 
 	private Long id;
 	private String phoneNumber;
 
 	// Constructor used when mapping the DTO response.
-	public CopayMemberDTO(Long id, String phoneNumber) {
+	public RegisteredMemberDTO(Long id, String phoneNumber) {
 		this.id = id;
 		this.phoneNumber = phoneNumber;
 	}
