ae4cb79cd574fbb156c56b081b390389ae754c15
Dynavy <dynavy@hotmail.com>
2025-04-16 17:49:50 +0200

    CHORE(UpdateGroupCopayMemberRequestDTO): Changed variable name

--- src/main/java/com/copay/app/dto/group/UpdateGroupCopayMembersRequestDTO.java
+++ src/main/java/com/copay/app/dto/group/UpdateGroupCopayMembersRequestDTO.java
@@ -1,19 +1,19 @@
 package com.copay.app.dto.group;
 
 import java.util.List;
-import com.copay.app.dto.group.auxiliary.CopayMemberDTO;
+import com.copay.app.dto.group.auxiliary.RegisteredMemberDTO;
 import jakarta.validation.constraints.NotNull;
 
 public class UpdateGroupCopayMembersRequestDTO {
 
 	@NotNull(message = "Copay members list must not be null.")
-	private List<String> invitedCopayMembers;
+	private List<String> invitedRegisteredMembers;
 
-	public List<String> getInvitedCopayMembers() {
-		return invitedCopayMembers;
+	public List<String> getRegisteredCopayMembers() {
+		return invitedRegisteredMembers;
 	}
 
-	public void setInvitedCopayMembers(List<String> invitedCopayMembers) {
-		this.invitedCopayMembers = invitedCopayMembers;
+	public void setRegisteredCopayMembers(List<String> invitedRegisteredMembers) {
+		this.invitedRegisteredMembers = invitedRegisteredMembers;
 	}
 }
