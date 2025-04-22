7ad18a5e9e42082f055a80e152e97b5a8dbb1444
Dynavy <dynavy@hotmail.com>
2025-04-22 19:45:44 +0200

    FEAT(ExternalMember): Added new relationships to UserExpense.
    - @OneToMany relationship debtorExternalMember to UserExpense table.
    
    - @OneToMany relationship debtorAsCreditos to UserExpense table.
    
    - Created new Getters and Setters.

--- src/main/java/com/copay/app/entity/relations/ExternalMember.java
+++ src/main/java/com/copay/app/entity/relations/ExternalMember.java
@@ -1,9 +1,22 @@
 package com.copay.app.entity.relations;
 
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.copay.app.entity.Group;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "external_members")
@@ -17,14 +30,21 @@ public class ExternalMember {
 	@Column(nullable = false)
 	private String name;
 
+	@Column(name = "joined_at", nullable = false)
+	private LocalDateTime joinedAt;
+	
 	// LAZY fetch is used to avoid unnecessary loading of external members if not accessed.
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "group_id", nullable = false)
 	private Group group;
 
-	@Column(name = "joined_at", nullable = false)
-	private LocalDateTime joinedAt;
+	// Bidirectional mappings for debts
+    @OneToMany(mappedBy = "debtorExternalMember", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserExpense> debtsAsDebtor = new ArrayList<>();
 
+    @OneToMany(mappedBy = "creditorExternalMember", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserExpense> debtsAsCreditor = new ArrayList<>();
+	
 	// Empty constructor.
 	public ExternalMember() {
 		this.joinedAt = LocalDateTime.now();
@@ -61,5 +81,21 @@ public class ExternalMember {
 
 	public void setJoinedAt(LocalDateTime joinedAt) {
 		this.joinedAt = joinedAt;
+	}
+
+	public List<UserExpense> getDebtsAsDebtor() {
+		return debtsAsDebtor;
+	}
+
+	public void setDebtsAsDebtor(List<UserExpense> debtsAsDebtor) {
+		this.debtsAsDebtor = debtsAsDebtor;
+	}
+
+	public List<UserExpense> getDebtsAsCreditor() {
+		return debtsAsCreditor;
+	}
+
+	public void setDebtsAsCreditor(List<UserExpense> debtsAsCreditor) {
+		this.debtsAsCreditor = debtsAsCreditor;
 	}
 }
