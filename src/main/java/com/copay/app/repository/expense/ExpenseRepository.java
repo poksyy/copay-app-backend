87dea003ba49411f2f9c3bb7763f00069325e55d
Dynavy <dynavy@hotmail.com>
2025-04-27 20:06:23 +0200

    FEAT(Repositories): New repositories for expense management.
    - ExpenseRepository for the management of the Expense entity.
    
    - UserExpenseRepository for the managemeent of the UserExpense entity.
    
    - PaymentConfirmationRepository for the management of the PaymentConfirmation entity.

---
+++ src/main/java/com/copay/app/repository/expense/ExpenseRepository.java
@@ -0,0 +1,11 @@
+package com.copay.app.repository.expense;
+
+import com.copay.app.entity.Expense;
+import com.copay.app.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    Optional<Expense> findByGroup(Group group);
+}
