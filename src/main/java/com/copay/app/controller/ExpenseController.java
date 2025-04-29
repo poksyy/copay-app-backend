b2368e5de1fa60f9a43b3c539e58a67f17928ed4
Dynavy <dynavy@hotmail.com>
2025-04-29 21:04:41 +0200

    REFACTOR(ExpenseController): Avoid validations in controller layer.
    - The DTO validatiosn are now managed by the ExceptionHandler.

--- src/main/java/com/copay/app/controller/ExpenseController.java
+++ src/main/java/com/copay/app/controller/ExpenseController.java
@@ -42,14 +42,7 @@ public class ExpenseController {
 
     // Endpoint to retrieve all the expenses.
     @GetMapping("/{groupId}")
-    public ResponseEntity<?> getExpenses(@PathVariable Long groupId, BindingResult result) {
-
-        ValidationErrorResponse validationResponse = ValidationService.validate(result);
-
-        // Validates the DTO annotations.
-        if (validationResponse != null) {
-            return ResponseEntity.badRequest().body(validationResponse);
-        }
+    public ResponseEntity<?> getExpenses(@PathVariable Long groupId) {
 
         List<ExpenseResponseDTO> getExpenses = expenseService.getExpenses(groupId);
 
@@ -58,20 +51,14 @@ public class ExpenseController {
 
     // Endpoint to retrieve details of one expense.
     @GetMapping("/{groupId}/{expenseId}")
-    public ResponseEntity<?> getExpense(@PathVariable Long groupId, Long expenseId, BindingResult result) {
-
-        ValidationErrorResponse validationResponse = ValidationService.validate(result);
-
-        // Validates the DTO annotations.
-        if (validationResponse != null) {
-            return ResponseEntity.badRequest().body(validationResponse);
-        }
+    public ResponseEntity<?> getExpense(@PathVariable Long groupId, Long expenseId) {
 
         ExpenseResponseDTO getExpense = expenseService.getExpense(groupId, expenseId);
 
         return ResponseEntity.ok(getExpense);
     }
 
+    // TODO: Implement delete expense when more than one expense in one group is implemented.
     @DeleteMapping("/{groupId}/{expenseId}")
     public ResponseEntity<?> deleteExpense(@PathVariable Long groupId, @PathVariable Long expenseId) {
 
@@ -79,5 +66,4 @@ public class ExpenseController {
 
         return ResponseEntity.ok("Expense deleted successfully.");
     }
-
 }
