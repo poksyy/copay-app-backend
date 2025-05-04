fea66ca34eea7d0b986176726f3e5facd4db8781
Pau Carrera <153611745+poksyy@users.noreply.github.com>
2025-05-05 01:57:46 +0200

    FEAT(Validation): Added the custom validation to invalid payer selection

---
+++ src/main/java/com/copay/app/exception/group/InvalidPayerSelectionException.java
@@ -0,0 +1,7 @@
+package com.copay.app.exception.group;
+
+public class InvalidPayerSelectionException extends RuntimeException {
+    public InvalidPayerSelectionException(String message) {
+        super(message);
+    }
+}
