51f5df870d026487e00978bde87fccde36c8fb64
Dynavy <dynavy@hotmail.com>
2025-05-08 18:44:55 +0200

    CHORE(ExpenseNotFoundException): Added serialVersionUID

--- src/main/java/com/copay/app/exception/expense/ExpenseNotFoundException.java
+++ src/main/java/com/copay/app/exception/expense/ExpenseNotFoundException.java
@@ -1,6 +1,9 @@
 package com.copay.app.exception.expense;
 
 public class ExpenseNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
     public ExpenseNotFoundException(String message) {
         super(message);
     }
