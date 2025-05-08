f55d490c648bc79c9f7b24569a83cf48185ebd71
Dynavy <dynavy@hotmail.com>
2025-05-08 16:11:16 +0200

    FIX(ForgotPasswordRequestDTO): Fixed annotation validations

--- src/main/java/com/copay/app/dto/password/request/ForgotPasswordRequestDTO.java
+++ src/main/java/com/copay/app/dto/password/request/ForgotPasswordRequestDTO.java
@@ -1,15 +1,12 @@
 package com.copay.app.dto.password.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 public class ForgotPasswordRequestDTO {
 
-    @NotBlank(message = "Password must not be null")
-    @Size(min = 8, message = "Password must be at least 8 characters long")
-    @Pattern(regexp = ".*[A-Z].*", message = "Password must contain at least one uppercase letter")
-    @Pattern(regexp = ".*\\d.*", message = "Password must contain at least one number")
+    @NotBlank(message = "Email must not be null")
+    @Email(message = "Invalid email format")
     private String email;
 
     public String getEmail() {
