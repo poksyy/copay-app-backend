5a76e707dd05a874301a5e5ddb436a4787b434d7
Dynavy <dynavy@hotmail.com>
2025-05-22 19:31:53 +0200

    feat(groupQueryService): Add layer for the group related query's

--- src/main/java/com/copay/app/service/query/GroupQueryService.java
+++ src/main/java/com/copay/app/service/query/GroupQueryService.java
@@ -3,6 +3,7 @@ package com.copay.app.service.query;
 import com.copay.app.entity.Group;
 import com.copay.app.entity.User;
 import com.copay.app.repository.GroupRepository;
+import com.copay.app.service.JwtService;
 import com.copay.app.validation.group.GroupValidator;
 import org.springframework.stereotype.Service;
 
@@ -16,17 +17,35 @@ public class GroupQueryService {
 
     private final GroupRepository groupRepository;
 
-    private final GroupValidator GroupValidator;
+    private final GroupValidator groupValidator;
 
-    public GroupQueryService(GroupRepository groupRepository, GroupValidator GroupValidator) {
+    private final UserQueryService userQueryService;
+
+    private final JwtService jwtService;
+
+    public GroupQueryService(GroupRepository groupRepository, GroupValidator groupValidator,
+                             UserQueryService userQueryService, JwtService jwtService) {
 
         this.groupRepository = groupRepository;
-        this.GroupValidator = GroupValidator;
+        this.groupValidator = groupValidator;
+        this.userQueryService = userQueryService;
+        this.jwtService = jwtService;
     }
 
     // Retrieves user by ID, validates through GroupValidator and returns the user object or custom exception.
     public Group getGroupById(Long id) {
 
-        return GroupValidator.validateGroupById(groupRepository.findById(id), id);
+        return groupValidator.validateGroupById(groupRepository.findById(id), id);
     }
+
+    public void validateGroupCreator(Group group, String token) {
+
+        // Extract the phone number thanks to the token identifier.
+        String phoneNumber = jwtService.getUserIdentifierFromToken(token);
+
+        User user = userQueryService.getUserByPhone(phoneNumber);
+
+        groupValidator.validateGroupCreator(group, user.getUserId());
+    }
+
 }
