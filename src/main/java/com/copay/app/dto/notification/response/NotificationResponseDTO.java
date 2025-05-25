be54d1e374cca4dc3ba4fbbb4791c0728c2bbae6
Dynavy <pl2023379@365.stucom.com>
2025-05-25 17:36:59 +0200

    chore(NotificationResponseDTO): Formated code

--- src/main/java/com/copay/app/dto/notification/response/NotificationResponseDTO.java
+++ src/main/java/com/copay/app/dto/notification/response/NotificationResponseDTO.java
@@ -6,9 +6,13 @@ import java.time.LocalDateTime;
 public class NotificationResponseDTO {
     
     private Long notificationId;
+
     private Long userId;
+
     private String message;
+
     private boolean isRead;
+
     private LocalDateTime createdAt;
     
     public NotificationResponseDTO() {
