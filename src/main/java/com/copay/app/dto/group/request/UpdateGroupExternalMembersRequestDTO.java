a0f10bd48afcc349956493e15c398262fe9a47d3
Dynavy <pl2023379@365.stucom.com>
2025-05-27 22:25:41 +0200

    feat(UpdateGroupExternalMemberRequestDTO): Add @Validation

--- src/main/java/com/copay/app/dto/group/request/UpdateGroupExternalMembersRequestDTO.java
+++ src/main/java/com/copay/app/dto/group/request/UpdateGroupExternalMembersRequestDTO.java
@@ -3,11 +3,13 @@ package com.copay.app.dto.group.request;
 import java.util.List;
 
 import com.copay.app.dto.group.auxiliary.ExternalMemberDTO;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public class UpdateGroupExternalMembersRequestDTO {
 
 	@NotNull(message = "External members list must not be null.")
+	@Valid
 	private List<ExternalMemberDTO> invitedExternalMembers;
 
 	public List<ExternalMemberDTO> getInvitedExternalMembers() {
