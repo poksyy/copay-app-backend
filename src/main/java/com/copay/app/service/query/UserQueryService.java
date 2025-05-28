0f0a643f8e63e92b7cad789fa5d4bebd5551714b
Dylan Navarro Vinyarta <dynavy@hotmail.com>
2025-05-28 18:45:08 +0200

    feat(userQueryService/UserValidator): Add new query and validator methods

--- src/main/java/com/copay/app/service/query/UserQueryService.java
+++ src/main/java/com/copay/app/service/query/UserQueryService.java
@@ -1,7 +1,11 @@
 package com.copay.app.service.query;
 
+import com.copay.app.entity.Group;
 import com.copay.app.entity.User;
+import com.copay.app.exception.user.UserNotFoundException;
+import com.copay.app.repository.GroupMemberRepository;
 import com.copay.app.repository.UserRepository;
+import com.copay.app.service.JwtService;
 import com.copay.app.validation.user.UserValidator;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +21,15 @@ public class UserQueryService {
 
     private final UserValidator userValidator;
 
-    public UserQueryService(UserRepository userRepository, UserValidator userValidator) {
+    private final GroupMemberRepository groupMemberRepository;
+    private final JwtService jwtService;
+
+    public UserQueryService(UserRepository userRepository, UserValidator userValidator, GroupMemberRepository groupMemberRepository, JwtService jwtService) {
 
         this.userRepository = userRepository;
         this.userValidator = userValidator;
+        this.groupMemberRepository = groupMemberRepository;
+        this.jwtService = jwtService;
     }
 
     // Retrieves user by ID, validates through userValidator and returns the user object or custom exception.
@@ -59,5 +68,28 @@ public class UserQueryService {
 
         // Delegates exception handling to UserValidator.
         userValidator.validateEmailExistence(exists, email);
+    }
+
+    // Checks user availability in the group via repository and delegates exception handling to UserValidator.
+    public void validateUserInGroup(Long userId, Long groupId) {
+
+        // Verifies existence with the repository.
+        boolean exists = groupMemberRepository.existsByIdUserUserIdAndIdGroupGroupId(userId, groupId);
+
+        // Delegates exception handling to UserValidator.
+        userValidator.validateUserInGroup(exists, userId, groupId);
+    }
+
+    // Compares if the userId is equal or not with the tokenId and delegates exception handling to UserValidator.
+    public void validateUserIdMatchesToken(Long userId, String token) {
+
+        // Extracts the phone number from the token.
+        String phoneNumber = jwtService.getUserIdentifierFromToken(token);
+
+        // Finds the user thanks to the phoneNumber and validates if it exists.
+        User userFromToken = getUserByPhone(phoneNumber);
+
+        // Delegates exception handling to UserValidator.
+        userValidator.validateUserIdMatchesToken(userId, userFromToken.getUserId());
     }
 }
--- src/main/java/com/copay/app/validation/user/UserValidator.java
+++ src/main/java/com/copay/app/validation/user/UserValidator.java
@@ -1,9 +1,12 @@
 package com.copay.app.validation.user;
 
 import com.copay.app.entity.User;
+import com.copay.app.exception.group.GroupAccessDeniedException;
+import com.copay.app.exception.token.UserIdNotMatchTokenException;
 import com.copay.app.exception.user.EmailAlreadyExistsException;
 import com.copay.app.exception.user.PhoneAlreadyExistsException;
 import com.copay.app.exception.user.UserNotFoundException;
+import com.copay.app.exception.user.UserPermissionException;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -47,6 +50,24 @@ public class UserValidator {
         if (exists) {
 
             throw new PhoneAlreadyExistsException("Phone number " + phoneNumber + " already exists");
+        }
+    }
+
+    // Checks if the user in the specified group and throws an exception if not found.
+    public void validateUserInGroup(boolean exists, Long userId, Long groupId) {
+
+        if (!exists) {
+
+            throw new GroupAccessDeniedException("User with ID " + userId + " does not belong to group with ID " + groupId);
+        }
+    }
+
+    // Checks if the provided path variable of userId matches the user token.
+    public void validateUserIdMatchesToken(Long pathUserId, Long tokenUserId)
+    {
+        if (!pathUserId.equals(tokenUserId)) {
+
+            throw new UserIdNotMatchTokenException("User with ID " + pathUserId + " does not match the token user ID " + tokenUserId);
         }
     }
 }
